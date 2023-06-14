a9b2c6940c0e2b53fc0cf26ccfc8304b5a60a5d7
B153Yogeshwari <yogeshwarideshmukh95@gmail.com>
2023-06-14 13:29:48 +0530

    project created

---
+++ CarApplication/src/test/java/com/cjc/project/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
